46c1d4213c9693fdc7dd257bf45c7f69bb529436
raaasin <nisarvskp@gmail.com>
2023-11-18 03:55:19 +0530

    some-work-done

--- js/script.js
+++ js/script.js
@@ -6,3 +6,15 @@ const box = document.getElementById('bar3');
 
 box.innerHTML =
   '<object width="100%" height="100%" type="text/html" data="../bar3.html"</object>';
+
+
+const map = document.getElementById('map');
+
+map.innerHTML =
+  '<object width="100%" height="100%" type="text/html" data="../map.html"</object>';
+
+  
+const col = document.getElementById('column');
+
+col.innerHTML =
+  '<object width="100%" height="100%" type="text/html" data="../column.html"</object>';
